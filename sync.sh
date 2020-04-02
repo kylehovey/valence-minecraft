5915bdd4e660b49dc607582a604c878c0f30a41a
kylehovey <khoveyc@gmail.com>
2020-04-01 22:36:40 -0400

    Added variable config for sync script

--- sync.sh
+++ sync.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cd ~/backups/
-rsync --bwlimit=200 -v -P -e ssh `ls -t | tail -n 1` backup:~/friendcraft/
+backup_dir=~/backups
+bandwith_limit_kbps=200
+backup_host="backup"
+remote_backup_dir="~/friendcraft/"
+
+pushd $backup_dir
+rsync --bwlimit=200 -v -P -e ssh `ls -t | tail -n 1` $backup_host:$remote_backup_dir
+popd
