87cf88bb9b1ec4fe28e6b112c92524c1fc4f79b8
kylehovey <khoveyc@gmail.com>
2020-04-01 21:48:04 -0400

    Added dynamic exclusion of files in backup script

--- backup.sh
+++ backup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-cd ~/backups/
+backup_count=5
+backup_dir=backups
+server_dir=server
+ignore_files=(
+	server/plugins/*
+)
+ignore_directives=()
+
+for path in ${ignore_files[@]}; do
+	ignore_directives+=(--exclude="$path");
+done
+
 name="$(date +"%Y-%m-%d--%H--%M--%S").backup.tar.gz"
 echo "Removing old backups"
-ls -t | tail -n +5 | xargs rm --
+ls -t | tail -n +$backup_count | xargs rm --
 echo "Backing up server..."
-pushd ~
-tar -cvzf backups/$name --exclude='server/plugins/*' server
+pushd
+tar -cvzf $backup_dir/$name "${exclude[@]}" $server_dir
 popd
 echo "Backed up server in file: $name"
