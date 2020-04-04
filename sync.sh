33f3d51584b4e6e7d591b4816f597e99760e5aa6
kylehovey <khoveyc@gmail.com>
2020-04-04 19:36:33 -0400

    Fixed sync job so that it sends the most recent backup

--- sync.sh
+++ sync.sh
@@ -14,7 +14,7 @@ t_stamp="$(date +"%Y-%m-%d--%H--%M--%S")"
 
 pushd $backup_dir
 echo "Starting rsync job: sending backup to server" | add_date >> $log_file
-file=`ls -t | tail -n 1`
+file=`ls -rt | tail -1`
 echo "Sending $file" | add_date >> $log_file
 rsync --bwlimit=200 -v -P -e ssh $file  $backup_host:$remote_backup_dir
 echo "done." | add_date >> $log_file
