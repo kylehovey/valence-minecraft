ac2fba2f6363314a93e2f0e858cf6a983014078f
kylehovey <khoveyc@gmail.com>
2020-04-04 19:34:39 -0400

    Added log files to backup and sync

--- backup.sh
+++ backup.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-backup_count=5
+add_date() {
+    while IFS= read -r line; do
+        printf '%s %s\n' "$(date)" "$line";
+    done
+}
+
+backup_count=7
+log_file=~/logs/backup.log
 backup_dir=~/backups
 server_dir=~/server
 exclusion_list=~/config/exclude_from_backup.txt
 
 name="$(date +"%Y-%m-%d--%H--%M--%S").backup.tar.gz"
 
+echo "Backing up server in file: $name" | add_date >> $log_file
 pushd $backup_dir
-echo "Removing old backups"
+echo "Removing old backups" | add_date >> $log_file
 ls -t | tail -n +$backup_count | xargs -I{} sh -c "rm {}"
 popd
 
 pushd ~
-echo "Backing up server..."
+echo "Backing up server..." | add_date >> $log_file
 tar -cvzf $backup_dir/$name --exclude-from=$exclusion_list $server_dir
 popd
 
-echo "Backed up server in file: $name"
+echo "Done!" | add_date >> $log_file
--- sync.sh
+++ sync.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+add_date() {
+    while IFS= read -r line; do
+        printf '%s %s\n' "$(date)" "$line";
+    done
+}
+
 backup_dir=~/backups
+log_file=~/logs/sync.log
 backup_host="backup"
 remote_backup_dir="~/friendcraft/"
+t_stamp="$(date +"%Y-%m-%d--%H--%M--%S")"
 
 pushd $backup_dir
-rsync --bwlimit=200 -v -P -e ssh `ls -t | tail -n 1` $backup_host:$remote_backup_dir
+echo "Starting rsync job: sending backup to server" | add_date >> $log_file
+file=`ls -t | tail -n 1`
+echo "Sending $file" | add_date >> $log_file
+rsync --bwlimit=200 -v -P -e ssh $file  $backup_host:$remote_backup_dir
+echo "done." | add_date >> $log_file
 popd
